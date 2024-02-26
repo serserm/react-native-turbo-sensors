97da5e2d0f80902d50f50f93cd8c6e8e92e40713
SE <se@develup.pro>
2024-02-26 10:02:06 +0200

    add ios listener

--- packages/react-native-turbo-sensors/ios/TurboSensors.h
+++ packages/react-native-turbo-sensors/ios/TurboSensors.h
@@ -1,12 +1,14 @@
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "RNTurboSensorsSpec.h"
+#import <React/RCTEventEmitter.h>
 
-@interface TurboSensors : NSObject <NativeTurboSensorsSpec>
+@interface TurboSensors : NSObject <NativeTurboSensorsSpec, RCTEventEmitter>
 #else
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface TurboSensors : NSObject <RCTBridgeModule>
+@interface TurboSensors : NSObject <RCTBridgeModule, RCTEventEmitter>
 #endif
 
 @end
--- packages/react-native-turbo-sensors/ios/TurboSensors.mm
+++ packages/react-native-turbo-sensors/ios/TurboSensors.mm
@@ -3,17 +3,25 @@
 @implementation TurboSensors
 RCT_EXPORT_MODULE()
 
+- (NSArray<NSString *> *)supportedEvents {
+  return @[@"myEvent"];
+}
+
+- (void)sendEvent:(NSString *)eventName body:(id)body {
+  [self sendEventWithName:eventName body:body];
+}
+
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
-RCT_EXPORT_METHOD(multiply:(double)a
-                  b:(double)b
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-    NSNumber *result = @(a * b);
-
-    resolve(result);
-}
+// RCT_EXPORT_METHOD(multiply:(double)a
+//                   b:(double)b
+//                   resolve:(RCTPromiseResolveBlock)resolve
+//                   reject:(RCTPromiseRejectBlock)reject)
+// {
+//     NSNumber *result = @(a * b);
+//
+//     resolve(result);
+// }
 
 // Don't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
